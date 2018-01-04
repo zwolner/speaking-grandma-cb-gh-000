e6a3cab24cfdaa172abb2d05f96f81564fce7c32
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-04 18:12:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -18,6 +18,5 @@ def speak_to_grandma(input)
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
-    
-    
-    
+  end
+end
